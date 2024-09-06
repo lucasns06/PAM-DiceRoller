c77471c9f9ca968c16a35355dc598f34c01cedc6
Lucas Nascimento <2006lucasns@gmail.com>
2024-09-06 19:48:45 -0300

    Initial commit

---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DiceRoller;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    //Sortear um número aleatório através do clique do botão
+    //Devolver o número sorteado para a interface
+    //Conseguir selecionar a quantidade de lados a partir do Picker
+
+}
+
