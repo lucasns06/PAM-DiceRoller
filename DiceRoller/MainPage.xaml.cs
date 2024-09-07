1f903af2db2ac71918380ab5724f58e504afd7e8
lucasns06 <2006lucasns@gmail.com>
2024-09-06 21:44:48 -0300

    Atividade completa

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -2,11 +2,21 @@
 
 public partial class MainPage : ContentPage
 {
+
     public MainPage()
     {
         InitializeComponent();
     }
+    
 
+    private void RollerButton_Clicked(object sender, EventArgs e)
+    {
+        int value = Convert.ToInt32(EntryPicker.SelectedItem);
+        Random rnd = new Random();
+        int ResultNumber = rnd.Next(value);
+        LabelResult.Text = ResultNumber.ToString();
+    }
+    
     //Sortear um número aleatório através do clique do botão
     //Devolver o número sorteado para a interface
     //Conseguir selecionar a quantidade de lados a partir do Picker
