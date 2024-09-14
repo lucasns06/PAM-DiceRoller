3894618d22731abbbe42bc7589a5ac96d14e4175
lucasns06 <2006lucasns@gmail.com>
2024-09-13 21:19:58 -0300

    About page

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -7,13 +7,29 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
-    
+    public class Dado
+    {
 
+        private int quantidadeDeDados;
+        private int modificador;
+
+        public int RolarDado()
+        {
+            int value = Convert.ToInt32(SidesPicker.SelectedItem.ToString());
+            int ResultNumber = new Random().Next(1, value + 1);
+            ResultLabel.Text = ResultNumber.ToString();
+        }
+    }
+    /*
+   Quantidade de dados : rolar o dado ex: qtddados = 3, vai rolar 3 vezes obvio
+    modificador > somar cada resultado pelo valor.
+     */
+
+    Dado dado = new Dado();
     private void RollerButton_Clicked(object sender, EventArgs e)
     {
-        int value = Convert.ToInt32(SidesPicker.SelectedItem.ToString());
-        int ResultNumber = new Random().Next(1, value + 1);
-        ResultLabel.Text = ResultNumber.ToString();
+        int qtdDados = Convert.ToInt32(EntryQtdDados.ToString());
+        dado.RolarDado();
     }
     
     //Sortear um número aleatório através do clique do botão
